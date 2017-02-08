68d99132cc540fb26a5a070f39dc3b11e436804f
Ethan Lowry <ethanmarkl@gmail.com>
2017-02-08 01:21:32 +0000

    Updated mix version number to 0.4.1

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule KeyTools.Mixfile do
       app: :key_tools,
       description: "Simple functions for coercing Elixir Maps: atomizing, snake_casing, etc.",
       source_url: "https://github.com/EthanML/key_tools_ex",
-      version: "0.4.0",
+      version: "0.4.1",
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
